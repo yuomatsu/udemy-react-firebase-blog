80818478e941bde3b1d34d443154b54252d52ba7
yuomatsu <07yuya08@gmail.com>
2022-12-29 12:20:47 +0900

    記事投稿レイアウト修正

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./CreatePost.scss"
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを記入" />
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea placeholder="記事内容を記入" ></textarea>
+        </div>
+        <button className="postButton">投稿する</button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
