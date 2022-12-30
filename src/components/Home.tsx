2ba32db01bcf2a022519437efe2d83796ac7b6e7
yuomatsu <07yuya08@gmail.com>
2022-12-30 16:58:47 +0900

    記事を取得、表示

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import "./Home.scss";
+import { db } from "../firebase";
+
+type postList = {
+  id: string;
+  author: { username: string; id: string };
+  postText: string;
+  title: string;
+};
 
 const Home = () => {
-  return <div>Home</div>;
+  // TODO: anyをやめたい
+  const [postList, setPostList] = useState<postList[] | any>([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  }, []);
+  return (
+    <div className="homePage">
+      {postList.map((post: postList) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { getAuth, signOut } from "firebase/auth";
-import { auth, provider } from "../firebase";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -13,13 +12,12 @@ const Logout = ({ setIsAuth }: Props) => {
     const auth = getAuth();
     signOut(auth)
       .then(() => {
-        // Sign-out successful.
         localStorage["isAuth"] = false;
         setIsAuth(false);
         navigate("/login");
       })
       .catch((error) => {
-        // An error happened.
+        console.log(error);
       });
   };
   return (
